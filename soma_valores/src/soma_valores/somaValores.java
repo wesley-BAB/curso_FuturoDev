e1c422a5464c8fd831192387e794fd0d55a70441
WesleyBenevides <48894542+wesley-BAB@users.noreply.github.com>
2025-02-25 09:45:53 -0300

    Soma de dois valores
    Soma de dois valores

---
+++ soma_valores/src/soma_valores/somaValores.java
@@ -0,0 +1,12 @@
+package soma_valores;
+
+public class somaValores {
+    public static void main(String[] args) {
+        int valor1 = 10;
+        int valor2 = 20;
+        int soma = valor1 + valor2;
+
+        System.out.println("A Soma de "+ valor1 + "+"+valor2+" = " +soma);
+
+    }
+}
